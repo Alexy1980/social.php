b4b0cc0e1b528129d905417b13f851e145a72945
Paul <test@mail.ru>
2018-02-07 21:32:21 +0400

    list 3-48 Delete tweet

--- assets/js/delete.js
+++ assets/js/delete.js
@@ -1,4 +1,20 @@
 $(function(){
+   $(document).on('click', '.deleteTweet', function(){
+       var tweet_id = $(this).data('tweet');
+       $.post('http://twitter/core/ajax/deleteTweet.php', {showPopup:tweet_id}, function(data){
+            $('.popupTweet').html(data);
+           $('.close-retweet-popup, .cancel-it').click(function(){
+               $('.retweet-popup').hide();
+           });
+
+           $(document).on('click', '.delete-it', function(){
+               $.post('http://twitter/core/ajax/deleteTweet.php', {deleteTweet:tweet_id}, function(){
+                   $('.retweet-popup').hide();
+                   location.reload();
+               });
+           });
+       });
+   });
    $(document).on('click', '.deleteComment', function(){
        var commentID = $(this).data('comment');
        var tweetID = $(this).data('tweet');
